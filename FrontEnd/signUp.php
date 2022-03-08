6364d85d4d6692ddce6f6dacd78ac46f9e206c78
Atul Jairam <shadesofaura@gmail.com>
2022-03-08 02:31:12 -0500

    initial setup for signUp.php

---
+++ FrontEnd/signUp.php
@@ -0,0 +1,57 @@
+<?php
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>RegistrationForm_v2 by Colorlib</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- MATERIAL DESIGN ICONIC FONT -->
+    <link rel="stylesheet" href="fonts/material-design-iconic-font/css/material-design-iconic-font.min.css">
+
+    <!-- STYLE CSS -->
+    <link rel="stylesheet" href="../css/signup_css/style.css">
+</head>
+
+<body>
+
+<div class="wrapper" style="background-image: url('../images/registration-form-2.jpg');">
+    <div class="inner">
+        <form action="">
+            <h3 style="color:white">Registration Form</h3>
+            <div class="form-group">
+                <div class="form-wrapper">
+                    <label class="font-color" for="">First Name</label>
+                    <input type="text" class="form-control">
+                </div>
+                <div class="form-wrapper">
+                    <label class="font-color" for="">Last Name</label>
+                    <input type="text" class="form-control">
+                </div>
+            </div>
+            <div class="form-wrapper">
+                <label class="font-color" for="">Email</label>
+                <input type="text" class="form-control">
+            </div>
+            <div class="form-wrapper">
+                <label class="font-color" for="">Password</label>
+                <input type="password" class="form-control">
+            </div>
+            <div class="form-wrapper">
+                <label class="font-color" for="">Confirm Password</label>
+                <input type="password" class="form-control">
+            </div>
+            <div class="checkbox">
+                <label class="font-color">
+                    <input class="font-color" type="checkbox"> I caccept the Terms of Use & Privacy Policy.
+                    <span class="checkmark"></span>
+                </label>
+            </div>
+            <button class="font-color">Register Now</button>
+        </form>
+    </div>
+</div>
+
+</body>
+</html>
