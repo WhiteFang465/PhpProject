718adb2f4a0e0fa0614bba2577cd5a030e3cf89b
Dhawal Sharma <dhawalsharma26@icloud.com>
2022-03-09 13:31:13 -0500

    Profile-Pictures with name added Home Page designed

---
+++ views/index.php
@@ -0,0 +1,82 @@
+<html>
+<?php require_once "./../includes/header.php";
+$demoProfile=[
+        ["name"=>"Aiony Haust","age"=>"24","imgSrc"=>"aiony-haust.jpg","gender"=>"Female"],
+        ["name"=>"Aleksandr Minakov","age"=>"25","imgSrc"=>"aleksandr-minakov.jpg","gender"=>"female"],
+        ["name"=>"Alexander Hipp","age"=>"20","imgSrc"=>"alexander-hipp.jpg","gender"=>"Male"],
+        ["name"=>"Christian Buehner","age"=>"25","imgSrc"=>"christian-buehner.jpg","gender"=>"Male"]
+]
+?>
+<body>
+<div id="parent_div">
+    <div class="header_navbar">
+        <div class="header_navbar_logo">
+<!--                                                info related to logo-->
+        </div>
+        <div class="header_navbar_contents">
+            <div class="header_navbar_contents_child">
+                <a href="index.php">Home</a>
+                <a href="#">Find</a>
+                <a href="#">About</a>
+                <a href="#">Register</a>
+            </div>
+        </div>
+    </div>
+    <div class="banner">
+    <div id="banner-image">
+        <div class="container">
+            <div id="banner_content">
+                <form class="form-group">
+                    <div class="form-group row">
+                        <label  class="col-sm-2 col-form-label">Looking For</label>
+                        <select class="form-select form-select-sm col-sm-6" name="looking_for_select">
+                        <option value="1">Female</option>
+                        <option value="2">Male</option>
+                        <option value="3">Bi-Sexual</option>
+                        <option value="4">Gay</option>
+                        </select>
+                    </div>
+
+                    <div class="form-group row">
+                        <label  class="col-sm-2 col-form-label">From Age</label>
+                        <input type="number" class="form-control col-sm-6" name="min_age" placeholder="Minimum Age">
+                    </div>
+
+                    <div class="form-group row">
+                        <label  class="col-sm-2 col-form-label">To Age</label>
+                        <input type="number" class="form-control col-sm-6" name="max_age" placeholder="Maximum Age">
+                    </div>
+                    <div class="form-group row">
+                        <div class="col-sm-6">
+                            <button type="submit" class="btn">Let's Go</button>
+                        </div>
+                    </div>
+
+                </form>
+            </div>
+        </div>
+    </div>
+    </div>
+    <div class="profiles_for_guest">
+        <div class="profiles_holder">
+        <?php
+        foreach ($demoProfile as $key)
+        {
+        ?>
+        <div class="profiles_for_guest_container">
+            <img src="../images/Profile_Pictures/<?=$key['imgSrc']?>" alt="<?=$key['name']?>" class="profiles_for_guest_image" style="width:100%;height: 210%">
+            <div class="profiles_for_guest_middle">
+                <div><h5 >Name : <?=$key['name']?> </h5>
+                     <h5>Age : <?=$key['age']?></h5>
+                     <h5>Gender : <?=$key['gender']?></h5>
+                     <button class="btn">Connect</button></div>
+            </div>
+        </div>
+        <?php
+        }
+        ?>
+        </div>
+    </div>
+</div>
+</body>
+</html>
