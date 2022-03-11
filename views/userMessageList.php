9f16488be5be3146d88b67bbef72146b98db065d
puru888 <papujara162@gmail.com>
2022-03-11 15:07:25 -0500

    user message list created

---
+++ views/userMessageList.php
@@ -0,0 +1,24 @@
+<?php
+?>
+
+<!doctype html>
+<html lang="en">
+<?php require_once "./../includes/header.php"?>
+<body>
+<div class="container">
+    <div class="row">
+        <div class="col-12 col-sm-8 col-lg-5">
+            <h6 class="text-muted">Send Messages</h6>
+            <ul class="list-group">
+                <li class="list-group-item d-flex align-items-center">
+                    <div class="image-parent">
+                        <img src="./../images/download.jpg" alt="quixote">
+                    </div>
+                    <a href="messagePage.php"><h5 style="margin-left: 3rem">Don Quixote</h5></a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
+</body>
+</html>
