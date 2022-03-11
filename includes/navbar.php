0cc1f9536b4c399e4ce8db452b3117492f2132f0
DikshaKatwani41 <dikshakhatwani98@gmail.com>
2022-03-10 20:16:00 -0500

    completed the functionality for search by connecting to the server

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,15 +1,13 @@
+<?php
+?>
 <div class="header_navbar">
     <div class="header_navbar_logo">
         <!--                                                info related to logo-->
-        <a href="../views/index.php"> <img src="../images/datein_logo.png" > </a>
-
     </div>
     <div class="header_navbar_contents">
         <div class="header_navbar_contents_child">
             <a href="../views/index.php">Home</a>
-            <a href="#">Find</a>
-            <a href="#">About</a>
-            <a href="#">Register</a>
+            <a href="../views/SignUpUI.php">Register</a>
         </div>
     </div>
 </div>
