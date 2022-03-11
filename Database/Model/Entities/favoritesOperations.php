52480d3735d5253c50da492a93389f52ab5b9ef9
DikshaKatwani41 <dikshakhatwani98@gmail.com>
2022-03-11 17:36:22 -0500

    created the operations regarding the favoritesOperations.php

---
+++ Database/Model/Entities/favoritesOperations.php
@@ -0,0 +1,49 @@
+<?php
+$_SESSION['id']=4;
+require_once "user.php";
+require_once "favorites.php";
+
+require_once "database.php";
+
+class FavoriteTable extends Database
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function addToFavorites(int $userID)
+    {
+        $query = "insert into favorites (user_id, favorite_persons_user_id, added, removed, is_read ) VALUES (?, ?, ?, ?,?)";
+        $values = [$_SESSION['id'], $userID, 1,0 , 0];
+        $idOfRow = $this->executeInsert( $query, $values);
+
+    }
+    public function favoriteRecordExists(int $userID) : bool
+    {
+        $query ="select * from favorites where user_id=? and favorite_persons_user_id=?";
+        $values = [$_SESSION['id'], $userID];
+        $result = $this->execute( $query, $values);
+        if($result==false){
+            return false;
+        }
+        else{
+            return true;
+        }
+
+    }
+    public function removeToFavorites(int $userID)
+    {
+        if($this->favoriteRecordExists($userID)) {
+
+            $query = "update favorites  SET added=0, removed=1, is_read=0 where user_id=? and favorite_persons_user_id=?";
+            $values = [$_SESSION['id'], $userID];
+            $this->execute($query, $values);
+        }
+    }
+
+
+
+}
+$favoritesTable = new FavoriteTable();
